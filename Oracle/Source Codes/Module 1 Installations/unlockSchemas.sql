6a6cfd0e66b668a1ac4a96059fc6e8eb8df04fd1
SC-Wintergreen <virtuallysagnik5@gmail.com>
2024-09-03 21:57:00 +0530

    Delete Oracle directory

--- Oracle/Source Codes/Module 1 Installations/unlockSchemas.sql
+++
@@ -1,6 +0,0 @@
-sqlplus / as sysdba; 
-alter session set container=orclpdb; 
-alter pluggable database open; 
-alter pluggable database orclpdb save state; 
-alter user hr identified by hr account unlock; 
-/
